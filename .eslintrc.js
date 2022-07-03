f39502a9615fdcef2f3ecd23852a6d4cef89d57f
lmarschall <lmarschall.com@gmail.com>
2022-07-03 02:40:29 +0200

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
+    "vue/multi-word-component-names": "off",
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
   },
